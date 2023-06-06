9979742c2b8d6d71c889b5c59420ca0bc013e9b0
micah614 <59854631+Micah614@users.noreply.github.com>
2023-06-05 17:48:34 -0800

    Migrated Arduino files from tutorials completed between May 28th to June 3rd.

---
+++ arduino/Flowing_LED_Display/Flowing_LED_Display.ino
@@ -0,0 +1,12 @@
+// Control_LED_by_Button
+
+/* Objective: Control an LED by associating it with a button. 
+*/
+
+int buttonPin = 2;  // 
+int ledPin = 13;  //
+
+void setup() {
+  pinMode(buttonPin, INPUT);  // 
+  pinMode(ledPin, OUTPUT);  // 
+}
