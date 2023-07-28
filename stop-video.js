ba740809ae09d7289bf59e12f57bca9d8941940b
micah614 <59854631+Micah614@users.noreply.github.com>
2023-07-28 12:07:33 -0800

    Add javascript to pause embedded videos on loading

---
+++ stop-video.js
@@ -0,0 +1,11 @@
+var stopVideo = function ( element ) {
+	var iframe = element.querySelector( 'iframe');
+	var video = element.querySelector( 'video' );
+	if ( iframe ) {
+		var iframeSrc = iframe.src;
+		iframe.src = iframeSrc;
+	}
+	if ( video ) {
+		video.pause();
+	}
+};
